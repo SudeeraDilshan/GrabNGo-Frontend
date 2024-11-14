2910fcf35484d902a121c58d8f06682c0471f399
Hareni Thayaparan <hareniextp@gmail.com>
2024-11-14 13:39:00 +0530

    Login Component Created

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+//import { FooterComponent } from '../footer/footer.component';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
