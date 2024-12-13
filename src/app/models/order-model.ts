c87db9f8ed956d4ac5447dd89b9c34e23eabfed5
Hareni Thayaparan <hareniextp@gmail.com>
2024-12-13 12:08:07 +0530

    Lates updates for commit

--- src/app/models/order-model.ts
+++ src/app/models/order-model.ts
@@ -1,26 +1,26 @@
-export interface Order {
-    orderId: number;
-    userId: number;
-    totalPrice: number;
-    status: string;
-    createdDateTime: string;
-    discount: number;
-    firstName: string;
-    lastName: string;
-    address: string;
-    apartment: string;
-    city: string;
-    country: string;
-    zipCode: string;
-    orderItems: OrderItem[];
-}
-
-export interface OrderItem {
-    orderItemId: number;
-    orderId: number;
-    productId: number;
-    quantity: number;
-    discount: number;
-    sellPrice: number;
-}
-
+export interface OrderItemDTO {
+    orderItemId: number; // Unique ID for the order item
+    orderId: number;     // ID of the associated order
+    productId: number;   // ID of the product
+    quantity: number;    // Quantity of the product
+    discount: number;    // Discount applied to the item
+    sellPrice: number;   // Selling price of the item
+  }
+  
+  export interface Order {
+    orderId: number;              // Unique ID for the order
+    userId: number;               // ID of the user who placed the order
+    totalPrice: number;           // Total price of the order
+    status: string;               // Status of the order (e.g., "Pending", "Completed")
+    createdDateTime: string;      // Date and time the order was created
+    discount: number;             // Discount applied to the order
+    firstName: string;            // First name of the user
+    lastName: string;             // Last name of the user
+    address: string;              // Shipping address
+    apartment: string;            // Apartment details (if applicable)
+    city: string;                 // City of the address
+    country: string;              // Country of the address
+    zipCode: string;              // ZIP/postal code
+    orderItems: OrderItemDTO[];   // List of items in the order
+  }
+  
